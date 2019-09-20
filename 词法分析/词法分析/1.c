6bdb376be8cbc2586eb59ca011a879a3629395d3
16061190 <1664334639@qq.com>
2019-09-21 04:25:08 +0800

    完成词法分析和初步测试

--- 词法分析/词法分析/1.c
+++ 词法分析/词法分析/1.c
@@ -1,51 +1,12 @@
-
-const int const_int1 = 1;  
-const int const_int2 = 2, const_int3 = 3, const_int0 = -0;  
-const char const_char1 = 'a';  
-const char const_char2 = 'b', const_char3 = 'c';  
-int var_int1;  
-int var_int2, var_int_array[10];  
-char var_char1;  
-char var_char2, var_char_array[10];  
-void printGlobalConsts(){  
-    printf(const_int1);  
-    printf(const_int2);  
-    printf(const_int3);  
-    printf(const_int0);  
-    printf(const_char1);  
-    printf(const_char2);  
-    printf(const_char3);  
-    return;  
-}  
-  
-void printGlobalVariables(){  
-    printf(var_int1);  
-    printf(var_int2);  
-    return;  
-}  
-void printfGlobalArrays(){  
-    const int length = 10;  
-    int i;  
-    i = 0;  
-    while(i < length){  
-        var_int_array[i] = i;  
-        printf(var_int_array[i]);  
-        i = i + 1;  
-    }  
-    return;  
-}  
-  
-void main(){  
-    int int1, int2;  
-    char char1, char2;  
-    int i;  
-    int n;  
-    n = int1 + 10;  
-  
-    scanf(int1);  
-    scanf(int2, char1, char2);  
-    printGlobalConsts();  
-    printGlobalVariables();  
-    printfGlobalArrays();  
-      
-} 
+const int const1 = 1, const2 = -100;
+const char const3 = '_';
+int change1;
+char change3;
+int gets1(int var1,int var2){
+change1 = var1 + var2;
+return (change1);
+}
+void main(){
+printf("Hello World");
+printf(gets1(10, 20));
+}
--- 词法分析/词法分析/词法分析.cpp
+++ 词法分析/词法分析/词法分析.cpp
@@ -1,319 +1,184 @@
+#pragma warning(disable:4996)
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
 #include <ctype.h>
-
-
-char token[100];
-char string1[500];//字符串 
-char ch;//last char
-int num, i;
-float real;
-int sym;
-char pro[22][20] = { "begin","end","if","then","else","const","var","array","of","integer",
-"real","char","procedure","function","repeat","until","for","to","downto","do","read","write"
-}; //保留字表
-enum SYMBOL {
-	POINT = 0,
-	PLUS = 1,
-	MINUS = 2,
-	STAR = 3,
-	DIVI = 4,
-	LPAR = 5,
-	RPAR = 6,
-	COLON = 7,
-	SEMI = 8,
-	ASSIGN = 9,
-	LMPAR = 10,
-	RMPAR = 11,
-	COMMA = 12,
-	LAB = 13,
-	RAB = 14,
-	EQU = 15,
-	NLT = 16,
-	NMT = 17,
-	NEQU = 18,
-	ID = 19,
-	INT = 20,
-	REAL = 21,
-	STRING = 22,
-	resultValue
-}symbol;
-
-void clearToken() {
-	for (i = 0; i < sizeof(token); i++) {
-		token[i] = '\0';
-	}
-}
-int isSpace(char a) {
-	if (a == ' ')return 1;
-	else return 0;
-}
-int isNewline(char a) {
-	if (a == '\n')return 1;
-	else return 0;
-}
-int isTab(char a) {
-	if (a == '\t')return 1;
-	else return 0;
-}
-int isLetter(char a) {
-	if ((a >= 'a'&&a <= 'z') || (a >= 'A') && a <= 'Z')return 1;
-	else return 0;
-}
-int isDigit(char a) {
-	if (a >= '0'&&a <= '9')return 1;
-	else return 0;
-}
-int isColon(char a) {
-	if (a == ':')return 1;
-	else return 0;
-}
-int isComma(char a) {
-	if (a == ',')return 1;
-	else return 0;
-}
-int isSemi(char a) {
-	if (a == ';')return 1;
-	else return 0;
-}
-int isEqu(char a) {
-	if (a == '=')return 1;
-	else return 0;
-}
-int isPlus(char a) {
-	if (a == '+')return 1;
-	else return 0;
-}
-int isMinus(char a) {
-	if (a == '-')return 1;
-	else return 0;
-}
-int isDivi(char a) {
-	if (a == '/')return 1;
-	else return 0;
-}
-int isStar(char a) {
-	if (a == '*')return 1;
-	else return 0;
-}
-int isLpar(char a) {
-	if (a == '(')return 1;
-	else return 0;
-}
-int isRpar(char a) {
-	if (a == ')')return 1;
-	else return 0;
-}
-int isPoint(char a) {
-	if (a == '.')return 1;
-	else return 0;
-}
-int isLmpar(char a) {
-	if (a == '[')return 1;
-	else return 0;
-}
-int isRmpar(char a) {
-	if (a == ']')return 1;
-	else return 0;
-}
-int isLab(char a) {
-	if (a == '<')return 1;
-	else return 0;
-}
-int isRab(char a) {
-	if (a == '>')return 1;
-	else return 0;
-}
-
-
-
-void catToken(char a) {
-	token[strlen(token)] = a;
-}
-void retract(char a) {
-	ungetc(a, stdin);
-}
-int reserver(char a[]) {
-	for (i = 0; i < sizeof(pro); i++) {
-		if (strcmp(a, pro[i]) == 0) {
-			return i + 1 + 22;
-		}
-	}
-	return 0;
-}
-int transNum(char a[]) {
-	return atoi(a);
-}
-double transreal(char a[]) {
-	return atof(a);
-}
-
-
-
-void error() {
-	printf("error!");
-}
-
+#include"词法分析.h"
 
 int getsym()
 {
-	clearToken();
-	while (isSpace(ch) || isNewline(ch) || isTab(ch))
-		ch = getchar();									//跳过空格、换行、tab 
+	clear();
+	if (isSpace(ch)) {
+		symbol = SPACE;
+		return 0;
+	}
+	if (isTab(ch)) {
+		symbol = TAB;
+		return 0;
+	}
+	if (isNewline(ch)) {
+		symbol = NEWLINE;
+		num_line++;
+		return 0;
+	}
 	if (isLetter(ch))
 	{
 		while (isLetter(ch) || isDigit(ch))
 		{
 			catToken(ch);
-			ch = getchar();
+			ch = fgetc(fp);
 		}
 		retract(ch);
 		int resultValue = reserver(token);
-		if (resultValue == 0)symbol = ID;
-		else symbol = (enum SYMBOL)resultValue;
+		if (resultValue == 0)symbol = IDENFR; //返回0，没识别到，是标识符
+		else symbol = (enum SYMBOL)resultValue; //保留字
 	}
 	else if (isDigit(ch))
 	{
-		int point_time = 0;
-		while (isDigit(ch) || isPoint(ch))
-		{
+		if (ch == '0') {
+			catToken(ch);
+			symbol = INTCON;
+			ch = fgetc(fp);
 			if (isDigit(ch)) {
-				catToken(ch);
-				ch = getchar();
+				error(0);
 			}
-			if (isPoint(ch)) {
-				if (point_time == 0) {
-					catToken(ch);
-					ch = getchar();
-					point_time++;
-				}
-				else error();
-			}
+			retract(ch);
+			int_get = 0;
+			return 0;
+		}
+		while (isDigit(ch))
+		{
+			catToken(ch);
+			ch = fgetc(fp);
 		}
 		retract(ch);
-
-		if (point_time == 0) {
-			// num = transNum(token);
-			symbol = INT;
-		}
-		if (point_time == 1) {
-			// real = transreal(token);
-			symbol = REAL;
-		}
-
-	}
-	else if (isColon(ch))
-	{
-		ch = getchar();
-		if (isEqu(ch)) symbol = ASSIGN;
-		else
-		{
-			retract(ch);
-			symbol = COLON;
-		}
+		int_get = transNum(token);
+		symbol = INTCON;
 	}
 	else if (isPlus(ch)) symbol = PLUS;
-	else if (isMinus(ch)) symbol = MINUS;
-	else if (isStar(ch)) symbol = STAR;
-	else if (isDivi(ch)) symbol = DIVI;
-	else if (isLpar(ch)) symbol = LPAR;
-	else if (isRpar(ch)) symbol = RPAR;
+	else if (isMinu(ch)) symbol = MINU;
+	else if (isMULT(ch)) symbol = MULT;
+	else if (isDIV(ch)) symbol = DIV;
+	else if (isLparent(ch)) symbol = LPARENT;
+	else if (isRparent(ch)) symbol = RPARENT;
+	else if (isLbrack(ch)) symbol = LBRACK;
+	else if (isRbrack(ch)) symbol = RBRACK;
+	else if (isLbrace(ch)) symbol = LBRACE;
+	else if (isRbrace(ch)) symbol = RBRACE;
 	else if (isComma(ch)) symbol = COMMA;
-	else if (isSemi(ch)) symbol = SEMI;
-	else if (isEqu(ch)) symbol = EQU;
-	else if (isPoint(ch)) symbol = POINT;
-	else if (isLmpar(ch)) symbol = LMPAR;
-	else if (isRmpar(ch)) symbol = RMPAR;
-	else if (isLab(ch)) {
-		ch = getchar();
-		if (isEqu(ch)) symbol = NMT;
-		else if (isRab(ch)) symbol = NEQU;
+	else if (isSemi(ch)) symbol = SEMICN;
+	else if (isAssign(ch)) {
+		ch = fgetc(fp);
+		if (isAssign(ch)) symbol = EQL;
 		else
 		{
+			symbol = ASSIGN;
 			retract(ch);
-			symbol = LAB;
 		}
 	}
-	else if (isRab(ch)) {
-		ch = getchar();
-		if (isEqu(ch)) symbol = NLT;
+	else if (isLss(ch)) {
+		ch = fgetc(fp);
+		if (isAssign(ch)) symbol = LEQ;
 		else
 		{
 			retract(ch);
-			symbol = RAB;
+			symbol = LSS;
+		}
+	}
+	else if (isGre(ch)) {
+		ch = fgetc(fp);
+		if (isAssign(ch)) symbol = GEQ;
+		else
+		{
+			retract(ch);
+			symbol = GRE;
 		}
 	}
 	else if (ch == '\"') {
-		ch = getchar();
+		ch = fgetc(fp);
+		string_get_length = 0;
 		while (ch == 32 || ch == 33 || ch >= 35 && ch <= 126) {
-			token[strlen(token)] = ch;
-			ch = getchar();
+			string_get[string_get_length] = ch;
+			string_get_length++;
+			ch = fgetc(fp);
 		}
-		if (ch != '\"') error();
-		symbol = STRING;
+		if (ch != '\"') error(0);
+		symbol = STRCON;
 	}
-	else error();
+	else if (isQuotation(ch)) {
+		ch = fgetc(fp);
+		if (isDigit(ch) || isLetter(ch) || (ch == '*') || (ch == '/') || (ch == '+') || (ch == '-'))symbol = CHARCON;
+		else error(0);
+		char_get = ch;
+		ch = fgetc(fp);
+		if (ch != '\'')error(0);
+	}
+	else if (isExclamation(ch)) {
+		ch = fgetc(fp);
+		if (isAssign(ch)) {
+			symbol = NEQ;
+		}
+	}
+	else error(0);
+	//printf("%d\n", (int)symbol);
 	return 0;
 }
-
 int main() {
-	ch = getchar();
-	while (!isPoint(ch)) {
+	fp = fopen("1.c", "r");
+	ch = fgetc(fp);
+	while (ch != EOF) {
 		getsym();
 		sym = (int)symbol;
-		printf("%d ", sym);
+		//printf("line %d ", num_line);
+		//printf("%d ", sym);
 		switch (sym) {
-		case 1:printf("PLUS +"); break;
-		case 2:printf("MINUS -"); break;
-		case 3:printf("STAR *"); break;
-		case 4:printf("DIVI /"); break;
-		case 5:printf("LPAR ("); break;
-		case 6:printf("RPAR )"); break;
-		case 7:printf("COLON :"); break;
-		case 8:printf("SEMI ;"); break;
-		case 9:printf("ASSIGN :="); break;
-		case 10:printf("LMPAR ["); break;
-		case 11:printf("RMPAR ]"); break;
-		case 12:printf("COMMA ,"); break;
-		case 13:printf("LAB <"); break;
-		case 14:printf("RAB >"); break;
-		case 15:printf("EQU ="); break;
-		case 16:printf("NLT >="); break;
-		case 17:printf("NMT <="); break;
-		case 18:printf("NEQU <>"); break;
-		case 19:printf("symbol %s", token); break;
-		case 20:printf("integer %s", token); break;
-		case 21:printf("real %s", token); break;
-		case 22:printf("string %s", token); break;
-		case 23:printf("begin"); break;
-		case 24:printf("end"); break;
-		case 25:printf("if"); break;
-		case 26:printf("then"); break;
-		case 27:printf("else"); break;
-		case 28:printf("const"); break;
-		case 29:printf("var"); break;
-		case 30:printf("array"); break;
-		case 31:printf("of"); break;
-		case 32:printf("integer"); break;
-		case 33:printf("real"); break;
-		case 34:printf("char"); break;
-		case 35:printf("procedure"); break;
-		case 36:printf("function"); break;
-		case 37:printf("repeat"); break;
-		case 38:printf("until"); break;
-		case 39:printf("for"); break;
-		case 40:printf("to"); break;
-		case 41:printf("downto"); break;
-		case 42:printf("do"); break;
-		case 43:printf("read"); break;
-		case 44:printf("write"); break;
+		case 1:printf("IDENFR %s\n",token); break;
+		case 2:printf("ITCON %d\n",int_get); break;
+		case 3:printf("CHARCON %c\n",char_get); break;
+		case 4:printf("STRCON %s\n",string_get); break;
+		case 5:printf("CONSTTK const\n"); break;
+		case 6:printf("INTTK int\n"); break;
+		case 7:printf("CHARTK char\n"); break;
+		case 8:printf("VOIDTK void\n"); break;
+		case 9:printf("MAINTK main\n"); break;
+		case 10:printf("IFTK if\n"); break;
+		case 11:printf("ELSETK else\n"); break;
+		case 12:printf("DOTK do\n"); break;
+		case 13:printf("WHILETK while\n"); break;
+		case 14:printf("FORTK for\n"); break;
+		case 15:printf("SCANFTK scanf\n"); break;
+		case 16:printf("PRINTFTK printf\n"); break;
+		case 17:printf("RETURNTK return\n"); break;
+		case 18:printf("PLUS +\n"); break;
+		case 19:printf("MINU -\n"); break;
+		case 20:printf("MULT *\n"); break;
+		case 21:printf("DIV /\n"); break;
+		case 22:printf("LLS <\n"); break;
+		case 23:printf("LEQ <=\n"); break;
+		case 24:printf("GRE >\n"); break;
+		case 25:printf("GEQ >=\n"); break;
+		case 26:printf("EQL ==\n"); break;
+		case 27:printf("NEQ !=\n"); break;
+		case 28:printf("ASSIGN =\n"); break;
+		case 29:printf("SEMICN ;\n"); break;
+		case 30:printf("COMMA .\n"); break;
+		case 31:printf("LPARENT (\n"); break;
+		case 32:printf("RPARENT )\n"); break;
+		case 33:printf("LBRACK [\n"); break;
+		case 34:printf("RBRACK ]\n"); break;
+		case 35:printf("LBRACE {\n"); break;
+		case 36:printf("RBRACE }\n"); break;
+
+		case 40:break;
+		case 41:break;
+		case 42:break;
 		}
-		printf("\n");
-		ch = getchar();
+		ch = fgetc(fp);
 	}
+
+	system("pause");
 	return 0;
 
 }
